087874311eafe48ea2adccc77a88134ffa271e56
Chen Shu <csw.dawn@gmail.com>
2019-12-03 22:18:28 +0800

    refactory

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/service/auth.service';
 import { Router } from '@angular/router';
-import { Product } from 'src/app/model/product';
-import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-home',
